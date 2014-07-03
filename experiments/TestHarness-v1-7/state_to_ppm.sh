f1556555042c41861ab51f1693759d501486865d
Leonard Seymore <leonard.seymore@gmail.com>
2014-07-03 14:36:29 +0200

    clusters

--- experiments/TestHarness-v1-7/state_to_ppm.sh
+++ experiments/TestHarness-v1-7/state_to_ppm.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 echo P3
-echo 19 27
+echo 19 28
 echo 256
 sed 's/ /0 0 0 /g;s/#/64 64 64 /g;s/\./127 127 127 /g;s/*/196 196 196 /g;s/A/0 0 255 /g;s/B/0 255 0 /g' $1
 cat footer.ppm.part
