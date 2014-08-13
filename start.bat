41d60f65c930d4c40ebcf4ac37b8d69d6d3ae0b5
Leonard Seymore <leonard.seymore@gmail.com>
2014-08-13 12:23:26 +0200

    Bleh

--- start.bat
+++ start.bat
@@ -1,1 +1,1 @@
-java -cp target/pacman-bot-1.0-SNAPSHOT.jar za.co.entelect.challenge.Main %1
+java -cp target\pacman-1.0-SNAPSHOT-jar-with-dependencies.jar za.co.entelect.challenge.MainTestHarness za.co.entelect.challenge.agents.Zebra %1
